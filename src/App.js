ad8c55c9b06533e0a546c009b268355207ada3d4
BarbuAndreiIulian <83908356+BarbuAndreiIulian@users.noreply.github.com>
2022-03-06 22:24:28 +0100

    Inputs-scrimba
    Inputs-scrimba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function Form() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+  });
+
+  function handleChange(el) {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [el.target.name]: el.target.value,
+      };
+    });
+  }
+
+  console.log(formData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="First Name"
+        name="firstName"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        name="firstName"
+        onChange={handleChange}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        onChange={handleChange}
+      />
+      <textarea
+        type="text"
+        placeholder="Comments"
+        name="comments"
+        onChange={handleChange}
+      />
     </div>
   );
 }
-
-export default App;
